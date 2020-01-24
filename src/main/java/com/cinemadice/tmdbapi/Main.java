975da32737ff6ac9829ae736e9526278a53e93e3
Rauno Paluvee <rauno.paluvee@guardtime.com>
2020-01-24 10:14:26 +0200

    Changed example of use

--- src/main/java/com/cinemadice/tmdbapi/Main.java
+++ src/main/java/com/cinemadice/tmdbapi/Main.java
@@ -2,11 +2,13 @@ package com.cinemadice.tmdbapi;
 
 import com.cinemadice.tmdbapi.methods.TmdbMovies;
 
+// Example of use
 public class Main {
 
-    // Example of use
+    private static final String API_KEY = "2cbb5a59b82c66b9a2cf4c7e71442fdb";
+
     public static void main(String[] args) {
-        TmdbApi tmdbApi = new TmdbApi("2cbb5a59b82c66b9a2cf4c7e71442fdb");
+        TmdbApi tmdbApi = new TmdbApi(API_KEY);
         TmdbMovies tmdbMovies = tmdbApi.getTmdbMovies();
 
         System.out.println(tmdbMovies.fetchRandomMovie());
