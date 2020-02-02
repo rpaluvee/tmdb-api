13d5ec4fe5a988d7ac0016a457ccd64d5d5f2001
Rauno Paluvee <rauno.paluvee@guardtime.com>
2020-02-03 00:42:48 +0200

    Added lombok @AllArgsConstructor for enums

--- src/main/java/com/cinemadice/tmdbapi/url/Endpoint.java
+++ src/main/java/com/cinemadice/tmdbapi/url/Endpoint.java
@@ -1,15 +1,13 @@
 package com.cinemadice.tmdbapi.url;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
 enum Endpoint {
     DISCOVER_MOVIE("/discover/movie");
 
     @Getter
     private String endpointUrl;
-
-    Endpoint(String endpointUrl) {
-        this.endpointUrl = endpointUrl;
-    }
 
 }
