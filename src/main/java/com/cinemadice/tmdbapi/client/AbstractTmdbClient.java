c7ce2c19a2e7f6e4eee8dc11db46062d5e54d399
Rauno Paluvee <rauno.paluvee@guardtime.com>
2020-02-05 00:15:07 +0200

    Added logger for a failed connection with TMDb API

--- src/main/java/com/cinemadice/tmdbapi/client/AbstractTmdbClient.java
+++ src/main/java/com/cinemadice/tmdbapi/client/AbstractTmdbClient.java
@@ -6,9 +6,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 // TODO: should rename this to something else
 abstract class AbstractTmdbClient {
+
+    private static Logger LOGGER = Logger.getLogger(AbstractTmdbClient.class.toString());
 
     <T> T fromJson(Reader reader, Class<T> clazz) {
         return new Gson().fromJson(reader, clazz);
@@ -18,7 +22,7 @@ abstract class AbstractTmdbClient {
         try {
             return new InputStreamReader(url.openStream());
         } catch (IOException e) {
-            // TODO: think bout it
+            LOGGER.log(Level.SEVERE,"Connection could not be established with URL: " + url.toString());
             throw new RuntimeException(e);
         }
     }
