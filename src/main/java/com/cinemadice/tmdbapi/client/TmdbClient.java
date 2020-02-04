ac4d6be54597c37e8249fc366893f6848cdb312f
Rauno Paluvee <rauno.paluvee@guardtime.com>
2020-02-05 00:11:45 +0200

    Added invalid API key handling

--- src/main/java/com/cinemadice/tmdbapi/client/TmdbClient.java
+++ src/main/java/com/cinemadice/tmdbapi/client/TmdbClient.java
@@ -6,6 +6,9 @@ public class TmdbClient {
     private final String apiKey;
 
     public TmdbClient(String apiKey) {
+        if (apiKey == null || apiKey.isEmpty()) {
+            throw new IllegalArgumentException("Provided API key is invalid");
+        }
         this.apiKey = apiKey;
     }
 
