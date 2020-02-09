8030f29027ceb7df763a6c9e60a7c9be0a706742
Rauno Paluvee <rauno.paluvee@guardtime.com>
2020-02-09 20:46:36 +0200

    Added new filed for refactored connection logic

---
+++ src/main/java/com/cinemadice/tmdbapi/client/TmdbHttpRequest.java
@@ -0,0 +1,35 @@
+package com.cinemadice.tmdbapi.client;
+
+import com.cinemadice.tmdbapi.exception.FailedTmdbRequestException;
+import com.cinemadice.tmdbapi.model.ErrorResponse;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+
+class TmdbHttpRequest {
+
+    private final OkHttpClient client = new OkHttpClient();
+
+    String fetch(URL url) {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            String responseBody = Objects.requireNonNull(response.body()).string();
+            if (response.isSuccessful()) {
+                return responseBody;
+            } else {
+                ErrorResponse errorResponse = Utils.fromJson(responseBody, ErrorResponse.class);
+                throw new FailedTmdbRequestException(response.code(), response.message(), errorResponse.getStatusMessage());
+            }
+        } catch (IOException e) {
+            throw new FailedTmdbRequestException("Connection could not be established with URL: " + url);
+        }
+    }
+
+}
---
+++ src/main/java/com/cinemadice/tmdbapi/model/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.cinemadice.tmdbapi.model;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+
+@Data
+public class ErrorResponse {
+
+    @SerializedName("status_code")
+    private int statusCode;
+    @SerializedName("status_message")
+    private String statusMessage;
+    private boolean success;
+
+}
