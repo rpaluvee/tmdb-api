fcc56ab8685ec6ae6dc17dff7eab0f531f6b6e4e
Rauno Paluvee <rauno.paluvee@guardtime.com>
2020-02-13 12:17:34 +0200

    Added remaining methods for discovering movies

--- src/main/java/com/cinemadice/tmdbapi/url/DiscoverMoviesUrl.java
+++ src/main/java/com/cinemadice/tmdbapi/url/DiscoverMoviesUrl.java
@@ -27,7 +27,7 @@ public class DiscoverMoviesUrl extends AbstractTmdbUrl {
         tmdbParameters.put(TmdbParameter.CERTIFICATION, String.valueOf(certification));
     }
 
-    public void addCertificationLesserThanOrEqual(String certification) {
+    public void addCertificationLessThanOrEqual(String certification) {
         tmdbParameters.put(TmdbParameter.CERTIFICATION_LTE, String.valueOf(certification));
     }
 
@@ -51,7 +51,7 @@ public class DiscoverMoviesUrl extends AbstractTmdbUrl {
         tmdbParameters.put(TmdbParameter.PRIMARY_RELEASE_YEAR, String.valueOf(year));
     }
 
-    public void addPrimaryReleaseDateLesserThanOrEqual(int primaryReleaseDate) {
+    public void addPrimaryReleaseDateLessThanOrEqual(int primaryReleaseDate) {
         tmdbParameters.put(TmdbParameter.PRIMARY_RELEASE_DATE_LTE, String.valueOf(primaryReleaseDate));
     }
 
@@ -59,7 +59,7 @@ public class DiscoverMoviesUrl extends AbstractTmdbUrl {
         tmdbParameters.put(TmdbParameter.PRIMARY_RELEASE_DATE_GTE, String.valueOf(primaryReleaseDate));
     }
 
-    public void addReleaseDateLesserThanOrEqual(int releaseDate) {
+    public void addReleaseDateLessThanOrEqual(int releaseDate) {
         tmdbParameters.put(TmdbParameter.RELEASE_DATE_LTE, String.valueOf(releaseDate));
     }
 
@@ -67,8 +67,68 @@ public class DiscoverMoviesUrl extends AbstractTmdbUrl {
         tmdbParameters.put(TmdbParameter.RELEASE_DATE_GTE, String.valueOf(releaseDate));
     }
 
-    public void addReleaseType(int releaseType) {
+    public void addWithReleaseType(int releaseType) {
         tmdbParameters.put(TmdbParameter.WITH_RELEASE_TYPE, String.valueOf(releaseType));
+    }
+
+    public void addVoteCountLessThanOrEqual(int voteCount) {
+        tmdbParameters.put(TmdbParameter.VOTE_COUNT_LTE, String.valueOf(voteCount));
+    }
+
+    public void addVoteCountGreaterThanOrEqual(int voteCount) {
+        tmdbParameters.put(TmdbParameter.VOTE_COUNT_GTE, String.valueOf(voteCount));
+    }
+
+    public void addVoteAverageLessThanOrEqual(int voteAverage) {
+        tmdbParameters.put(TmdbParameter.VOTE_AVERAGE_LTE, String.valueOf(voteAverage));
+    }
+
+    public void addVoteAverageGreaterThanOrEqual(int voteAverage) {
+        tmdbParameters.put(TmdbParameter.VOTE_AVERAGE_GTE, String.valueOf(voteAverage));
+    }
+
+    public void addWithCast(String cast) {
+        tmdbParameters.put(TmdbParameter.WITH_CAST, cast);
+    }
+
+    public void addWithCrew(String crew) {
+        tmdbParameters.put(TmdbParameter.WITH_CREW, crew);
+    }
+
+    public void addWithPeople(String people) {
+        tmdbParameters.put(TmdbParameter.WITH_PEOPLE, people);
+    }
+
+    public void addWithCompanies(String companies) {
+        tmdbParameters.put(TmdbParameter.WITH_COMPANIES, companies);
+    }
+
+    public void addWithGenres(String genres) {
+        tmdbParameters.put(TmdbParameter.WITH_GENRES, genres);
+    }
+
+    public void addWithoutGenres(String genres) {
+        tmdbParameters.put(TmdbParameter.WITHOUT_GENRES, genres);
+    }
+
+    public void addWithKeywords(String keywords) {
+        tmdbParameters.put(TmdbParameter.WITH_KEYWORDS, keywords);
+    }
+
+    public void addWithoutKeywords(String keywords) {
+        tmdbParameters.put(TmdbParameter.WITHOUT_KEYWORDS, keywords);
+    }
+
+    public void addWithRuntimeLessThanOrEqual(int runtime) {
+        tmdbParameters.put(TmdbParameter.WITH_RUNTIME_LTE, String.valueOf(runtime));
+    }
+
+    public void addWithRuntimeGreaterThanOrEqual(int runtime) {
+        tmdbParameters.put(TmdbParameter.WITH_RUNTIME_GTE, String.valueOf(runtime));
+    }
+
+    public void addWithOriginalLanguage(String language) {
+        tmdbParameters.put(TmdbParameter.WITH_ORIGINAL_LANGUAGE, language);
     }
 
 }
