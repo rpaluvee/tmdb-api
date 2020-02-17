401e39a20cd483f459d8f7e20852e7f885252915
Rauno Paluvee <rauno.paluvee@guardtime.com>
2020-02-18 01:07:46 +0200

    Fixed casting problem in AbstractTmdbDiscoverRequest

--- src/main/java/com/cinemadice/tmdbapi/client/AbstractTmdbDiscoverRequest.java
+++ src/main/java/com/cinemadice/tmdbapi/client/AbstractTmdbDiscoverRequest.java
@@ -1,16 +1,15 @@
 package com.cinemadice.tmdbapi.client;
 
 import com.cinemadice.tmdbapi.url.AbstractDiscoverUrl;
-import com.cinemadice.tmdbapi.url.AbstractTmdbUrl;
 
-abstract class AbstractTmdbDiscoverRequest<T, U extends AbstractTmdbUrl> extends AbstractTmdbRequest<T, U> {
+abstract class AbstractTmdbDiscoverRequest<T, U extends AbstractDiscoverUrl> extends AbstractTmdbRequest<T, U> {
 
     protected AbstractTmdbDiscoverRequest(U tmdbUrl, TmdbHttpClient tmdbHttpClient) {
         super(tmdbUrl, tmdbHttpClient);
     }
 
     public T withPage(int page) {
-        ((AbstractDiscoverUrl) tmdbUrl).addPage(page);
+        tmdbUrl.addPage(page);
         return thisInstance();
     }
 
