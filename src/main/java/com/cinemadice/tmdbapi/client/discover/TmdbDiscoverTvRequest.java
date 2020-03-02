c06d07563cf1c1680381c3ba6da2a44f5626bc56
Rauno Paluvee <rauno.paluvee@guardtime.com>
2020-03-02 11:16:23 +0200

    Added methods for TV discover request

--- src/main/java/com/cinemadice/tmdbapi/client/discover/TmdbDiscoverTvRequest.java
+++ src/main/java/com/cinemadice/tmdbapi/client/discover/TmdbDiscoverTvRequest.java
@@ -13,8 +13,48 @@ public class TmdbDiscoverTvRequest extends AbstractTmdbDiscoverRequest<TmdbDisco
         super(new DiscoverTvUrl(), tmdbHttpClient);
     }
 
+    public TmdbDiscoverTvRequest withAirDateLessThanOrEqual(String airDate) {
+        tmdbUrl.addAirDateLessThanOrEqual(airDate);
+        return this;
+    }
+
+    public TmdbDiscoverTvRequest withAirDateGreaterThanOrEqual(String airDate) {
+        tmdbUrl.addAirDateGreaterThanOrEqual(airDate);
+        return this;
+    }
+
+    public TmdbDiscoverTvRequest withFirstAirDateLessThanOrEqual(String firstAirDate) {
+        tmdbUrl.addFirstAirDateLessThanOrEqual(firstAirDate);
+        return this;
+    }
+
+    public TmdbDiscoverTvRequest withFirstAirDateGreaterThanOrEqual(String firstAirDate) {
+        tmdbUrl.addFirstAirDateGreaterThanOrEqual(firstAirDate);
+        return this;
+    }
+
+    public TmdbDiscoverTvRequest withFirstAirDateYear(int firstAirDateYear) {
+        tmdbUrl.addFirstAirDateYear(firstAirDateYear);
+        return this;
+    }
+
     public TmdbDiscoverTvRequest withTimezone(String timezone) {
         tmdbUrl.addTimezone(timezone);
+        return this;
+    }
+
+    public TmdbDiscoverTvRequest withNetworks(String networks) {
+        tmdbUrl.addWithNetworks(networks);
+        return this;
+    }
+
+    public TmdbDiscoverTvRequest includeNullFirstAirDates(boolean hasAirDate) {
+        tmdbUrl.addIncludeNullFirstAirDates(hasAirDate);
+        return this;
+    }
+
+    public TmdbDiscoverTvRequest screenedTheatrically(boolean hasScreenedTheatrically) {
+        tmdbUrl.addScreenedTheatrically(hasScreenedTheatrically);
         return this;
     }
 
