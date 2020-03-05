4364cc9d67533714b85ef798fd502bd4dfb74757
Rauno Paluvee <rauno.paluvee@guardtime.com>
2020-03-05 23:03:07 +0200

    Added unit test for TMDb movie details request

---
+++ src/test/java/com/cinemadice/tmdbapi/client/movies/TmdbMovieDetailsRequestTest.java
@@ -0,0 +1,61 @@
+package com.cinemadice.tmdbapi.client.movies;
+
+import com.cinemadice.tmdbapi.client.TmdbHttpClient;
+import com.cinemadice.tmdbapi.model.movies.MovieDetails;
+import com.cinemadice.tmdbapi.url.movies.MovieDetailsUrl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class TmdbMovieDetailsRequestTest {
+
+    TmdbMovieDetailsRequest tmdbMovieDetailsRequest;
+
+    @Mock
+    TmdbHttpClient tmdbHttpClient;
+
+    @BeforeEach
+    public void setUp() {
+        tmdbMovieDetailsRequest = new TmdbMovieDetailsRequest(new MovieDetailsUrl(1), tmdbHttpClient);
+    }
+
+    @Test
+    public void fetchesWithNoParameters() throws MalformedURLException {
+        // given
+        MovieDetails expected = new MovieDetails();
+        URL expectedUrl = new URL("https://api.themoviedb.org/3/movie/1?");
+        when(tmdbHttpClient.fetch(expectedUrl, MovieDetails.class)).thenReturn(expected);
+
+        // when
+        MovieDetails actual = tmdbMovieDetailsRequest.fetch();
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void fetchesWithAllParameters() throws MalformedURLException {
+        // given
+        MovieDetails expected = new MovieDetails();
+        URL expectedUrl = new URL("https://api.themoviedb.org/3/movie/1?language=test");
+        when(tmdbHttpClient.fetch(expectedUrl, MovieDetails.class)).thenReturn(expected);
+
+        // when
+        MovieDetails actual = tmdbMovieDetailsRequest
+                .withLanguage("test")
+                .fetch();
+
+        // then
+        assertEquals(expected, actual);
+    }
+
+}
