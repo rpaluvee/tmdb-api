2e8b0a6118976ff537e9456338774c34de3368a9
Rauno Paluvee <rauno.paluvee@guardtime.com>
2020-03-05 23:00:56 +0200

    Added unit test for TMDb Discover Client

---
+++ src/test/java/com/cinemadice/tmdbapi/client/discover/TmdbDiscoverClientTest.java
@@ -0,0 +1,42 @@
+package com.cinemadice.tmdbapi.client.discover;
+
+import com.cinemadice.tmdbapi.client.TmdbHttpClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class TmdbDiscoverClientTest {
+
+    TmdbDiscoverClient tmdbDiscoverClient;
+
+    @Mock
+    TmdbHttpClient tmdbHttpClient;
+
+    @BeforeEach
+    public void setUp() {
+        tmdbDiscoverClient = new TmdbDiscoverClient(tmdbHttpClient);
+    }
+
+    @Test
+    public void throwsExceptionWhenGivenNull() {
+        assertThrows(IllegalArgumentException.class, () -> new TmdbDiscoverClient(null));
+    }
+
+    @Test
+    public void initializesMoviesRequest() {
+        TmdbDiscoverMoviesRequest result = tmdbDiscoverClient.movies();
+        assertNotNull(result);
+    }
+
+    @Test
+    public void initializesTvRequest() {
+        TmdbDiscoverTvRequest result = tmdbDiscoverClient.tv();
+        assertNotNull(result);
+    }
+
+}
