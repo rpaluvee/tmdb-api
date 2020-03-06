4a4f036778e6b841388ac45a428ba02d739775f3
Rauno Paluvee <rauno.paluvee@guardtime.com>
2020-03-06 13:24:58 +0200

    Added checkstyle for tests

--- src/test/java/com/cinemadice/tmdbapi/client/discover/TmdbDiscoverMoviesRequestTest.java
+++ src/test/java/com/cinemadice/tmdbapi/client/discover/TmdbDiscoverMoviesRequestTest.java
@@ -15,7 +15,7 @@ import java.net.URL;
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
