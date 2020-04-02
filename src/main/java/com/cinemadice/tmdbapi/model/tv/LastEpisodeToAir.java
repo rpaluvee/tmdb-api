c248f99d80b1ea629cc2237cf0a635e5bb916e52
Rauno Paluvee <rauno.paluvee@guardtime.com>
2020-04-03 00:40:09 +0300

    Fixed access modifiers

--- src/main/java/com/cinemadice/tmdbapi/model/tv/LastEpisodeToAir.java
+++ src/main/java/com/cinemadice/tmdbapi/model/tv/LastEpisodeToAir.java
@@ -22,8 +22,8 @@ public class LastEpisodeToAir {
     @SerializedName("still_path")
     private String stillPath;
     @SerializedName("vote_average")
-    protected double voteAverage;
+    private double voteAverage;
     @SerializedName("vote_count")
-    protected int voteCount;
+    private int voteCount;
 
 }
